68471a0a1538ecf37d62347baf3c5cbc493ce67e
crack <crack@crack.com>
2023-01-07 09:56:13 +0800

    second commit

--- src/main/java/com/crack/git/GitTest.java
+++ src/main/java/com/crack/git/GitTest.java
@@ -6,5 +6,6 @@ package com.crack.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
